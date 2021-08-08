973c6ea40e16a0d6b1f1c9574a348e61f333a086
Mira <sma88000@gmail.com>
2021-08-08 23:19:01 +0500

    add main py file

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ import datetime
 start_time = datetime.datetime.now()
 print(f'Программа начала свою работу')
 
+finish_time = datetime.datetime.now()
+run_time = finish_time - start_time
 print('Программа закончила работу')
 print(f'Программа выполнялась {str(run_time)[:9]} секунды')
