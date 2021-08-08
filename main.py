f360f06cda0af540b1c493ec2dd8a3b4a59081e4
Mira <sma88000@gmail.com>
2021-08-08 23:17:24 +0500

    add main py fi;e

---
+++ main.py
@@ -0,0 +1,7 @@
+import datetime
+
+start_time = datetime.datetime.now()
+print(f'Программа начала свою работу')
+
+print('Программа закончила работу')
+print(f'Программа выполнялась {str(run_time)[:9]} секунды')
